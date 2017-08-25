339ec8e6338924763c48678e5b88745efcb6166a
DarushDev <darushfaqiri@gmail.com>
2017-08-26 00:18:56 +0530

    Register broadcast receiver for chaning time stamp

--- app/src/main/java/com/example/myandroidkotlinactivity/MainActivity.kt
+++ app/src/main/java/com/example/myandroidkotlinactivity/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.example.myandroidkotlinactivity
 
 import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private val ADD_TASK_REQUEST = 1
     private val taskList: MutableList<String> = mutableListOf()
     private val adapter by lazy { makeAdapter(taskList) }
+    private val tickReceiver by lazy { makeBroadcastReceiver() }
 
+    companion object {
+        private const val LOG_TAG = "MainActivityLog"
+
+        private fun getCurrentTimeStamp(): String {
+            val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.US)
+            val now = Date()
+            return simpleDateFormat.format(now)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +39,26 @@ class MainActivity : AppCompatActivity() {
         taskListView.adapter = adapter
 
         taskListView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id -> }
+    }
+
+    override fun onResume() {
+        // 1
+        super.onResume()
+        // 2
+        dateTimeTextView.text = getCurrentTimeStamp()
+        // 3
+        registerReceiver(tickReceiver, IntentFilter(Intent.ACTION_TIME_TICK))
+    }
+
+    override fun onPause() {
+        // 4
+        super.onPause()
+        // 5
+        try {
+            unregisterReceiver(tickReceiver)
+        } catch (e: IllegalArgumentException) {
+            Log.e(MainActivity.LOG_TAG, "Time tick Receiver not registered", e)
+        }
     }
 
     fun addTaskClicked(view: View) {
@@ -43,6 +79,16 @@ class MainActivity : AppCompatActivity() {
                     taskList.add(task)
                     // 4
                     adapter.notifyDataSetChanged()
+                }
+            }
+        }
+    }
+
+    private fun makeBroadcastReceiver(): BroadcastReceiver {
+        return object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent?) {
+                if (intent?.action == Intent.ACTION_TIME_TICK) {
+                    dateTimeTextView.text = getCurrentTimeStamp()
                 }
             }
         }
