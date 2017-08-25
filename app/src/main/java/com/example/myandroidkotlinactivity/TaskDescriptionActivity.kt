e34206cffd995247a8b2395659f8ec2946554e2b
DarushDev <darushfaqiri@gmail.com>
2017-08-25 23:47:50 +0530

    Add TaskDescriptionActivity class and layout content

--- app/src/main/java/com/example/myandroidkotlinactivity/TaskDescriptionActivity.kt
+++ app/src/main/java/com/example/myandroidkotlinactivity/TaskDescriptionActivity.kt
@@ -2,11 +2,23 @@ package com.example.myandroidkotlinactivity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class TaskDescriptionActivity : AppCompatActivity() {
 
+    // 1
+    companion object {
+        val EXTRA_TASK_DESCRIPTION = "task"
+    }
+
+    // 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_description)
+    }
+
+    // 3
+    fun doneClicked(view: View) {
+
     }
 }
